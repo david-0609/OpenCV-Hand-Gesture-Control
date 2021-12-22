2cb108dae53f82e75dc4b20a72a206fbfce8d45b
david-0609 <daidavid0609@gmail.com>
2021-12-22 21:15:38 +0100

    Optimsed is_up function, added TODO tasks

--- HandDetector.py
+++ HandDetector.py
@@ -3,6 +3,12 @@ import mediapipe as mp
 import time
 from collections import namedtuple
 
+'''
+TODO
+- Have the Finger class functional
+- Make the is_up function functional
+- Initialise Fingers from a config file
+'''
 class HandDetector():
     
     def __init__(self, mode = False, max_hands = 2):
@@ -43,16 +49,18 @@ class HandDetector():
 class Finger():
     
     def __init__(self, ids: list) -> None:
-        self.ids = lambda ids : ids.sort()
-        self.tip = self.ids[0]
+        self.ids = ids
 
     @property
     def is_up(self):
         
         Point = namedtuple("Point", ["x", "y"])
-        finger_ids = self.ids.sort()
+        finger_ids = self.ids
+        finger_ids = finger_ids.sort()
         ptList = []
         cleanLMlist = []
+        result_list = []
+
         
         for coord in lmIDlist:
             for id in finger_ids:
@@ -67,19 +75,20 @@ class Finger():
             ptList.append(pt)
         previous_y = None
         
-        times_run = 0
-        result_list = []
-        for point in ptList:
-            curr_y = point[1]
-            if times_run == 0:
-                previous_y = curr_y
-                times_run += 1
-            elif times_run > 0 and curr_y != previous_y and previous_y < curr_y:
-                print("true")
-                result_list.append(True)
-                times_run +=1
         
-        if all(x == True for x in result_list):
+        for i in len(ptList):
+            first_coord = ptList[i] 
+            second_coord = ptList[i+1]
+            # y of First Coord should be smaller than the second
+            try:
+                if first_coord.y < second_coord.y:
+                    result_list.append(True)
+                else:
+                    result_list.append(False)
+            except IndexError:
+                break
+        
+        if all(result_list):
             return True
         else:
             return False
@@ -109,7 +118,7 @@ def main():
             print(findLandmark(lmList, [4, 8, 12, 16, 20]))
         except IndexError:
             print("Not Found")
-        
+                
         currTime = time.time()
         fps = 1 / (currTime-prevTime)
         prevTime = currTime
