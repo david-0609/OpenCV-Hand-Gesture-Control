eb6f3658cca3c7cbbc9c807ec45e8c01fd61eb0b
david-0609 <daidavid0609@gmail.com>
2021-12-29 10:43:32 +0100

    Finished __start_detection

--- modules/Gesture.py
+++ modules/Gesture.py
@@ -1,22 +1,26 @@
 from pyautogui import keyDown, press, keyUp
-import os, sys
+import os, sys, time
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
 
-from main import FingerList
+from main import FingerList, logging_list
 
+error_log = []
 
 class Gesture():
 
-    def __init__(self, action: list, fingers_up: list, direction: str, detection_time = 5) -> None:
+    
+    def __init__(self, action: list, fingers_up: list, direction: str, detection_time = 3) -> None:
         self.action = action
         self.fingers_up = fingers_up
         self.direction = direction
         self.detection_time = detection_time
     
     def exec_action(self):
-    
+        
+        global error_log
+        
         try:
             for key in action:                
                 keyDown(key)
@@ -26,6 +30,10 @@ class Gesture():
                 
         except BaseException as e:
             print("Error: ", e)
+            error_log.append(e)
+        
+        finally:
+            return error_log
             
     def __start_detection(self):
         fingers_up = []
@@ -33,6 +41,14 @@ class Gesture():
         for finger in FingerList:
             if finger.is_up == True:
                 fingers_up.append(finger.is_up)
+        if len(fingers_up) == 5:
+            start = time.time()
+            while int(time.time())-start < 3:
+                detection_frames.append(logging_list[-1])
+                if len(fingers_up) == 0:
+                    break
+        return detection_frames
+                
 
     
             
