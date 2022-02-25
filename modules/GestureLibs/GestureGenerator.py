a9058359371597fd1f6d268096b5b940a1cb6cb0
david-0609 <daidavid0609@gmail.com>
2022-02-25 20:47:08 +0100

    Fixed use of ConfigParser

--- modules/GestureLibs/GestureGenerator.py
+++ modules/GestureLibs/GestureGenerator.py
@@ -8,16 +8,16 @@ class GestureGenerator:
 
     def read_config(self):
         GestureList = []
-        config = ConfigParser
+        config = ConfigParser()
         config.read(filenames=str(self.config_file))
         for section in config.sections():
             # Validates if all information in config can be used, raise errors that can be handled in run
             
-            gesture_name = config[section][name]
-            gesture_action = config[section][action]
-            gesture_fingers_up = config[section][fingers_up]
-            gesture_direction = config[section][direction]
-            GestureList.append(Gesture(str(gesture_name), gesture_action.split(","), str(gesture_fingers_up), str(gesture_direction)))           
+            gesture_name = config[section]["name"]
+            gesture_action = config[section]["action"]
+            gesture_fingers_up = config[section]["fingers_up"]
+            gesture_direction = config[section]["direction"]
+            GestureList.append(Gesture(str(gesture_name), gesture_action.split(","), gesture_fingers_up.split(","), str(gesture_direction)))
         return GestureList
 
     def write_config(self, name, fingers_up, direction, action):
